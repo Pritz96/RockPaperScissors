1e5ac650e08b2d08f8ebac6b4cfa12d06dc8f65b
Pritz96 <pritam1996@hotmail.co.uk>
2020-05-11 10:11:17 +0100

    added bo3 capability
    Signed-off-by: Pritz96 <pritam1996@hotmail.co.uk>

--- RPS.py
+++ RPS.py
@@ -6,32 +6,51 @@ def isUserInputValid(userChoice):
     else:
         return "false"
 
-userInput = ""
-while isUserInputValid(userInput)=="false":
-    userInput = input("Type 'rock', 'paper' or 'scissors':")
-    
-randomNumber = random.randrange(0, 2)
+userScore=0
+computerScore=0
+while (userScore != 3 and computerScore != 3):
+    print("User score: " + str(userScore))
+    print("Computer score: " + str(computerScore))
+    userInput = ""
+    while (isUserInputValid(userInput)=="false"):
+        userInput = input("Type 'rock', 'paper' or 'scissors':")
+        
+    randomNumber = random.randrange(0, 2)
 
-options = {
-    0 : "rock",
-    1 : "paper",
-    2 : "scissors"
-}
+    options = {
+        0 : "rock",
+        1 : "paper",
+        2 : "scissors"
+    }
 
-computersChoice = options[randomNumber]
-print("The computer randomly chose " + computersChoice)
+    computersChoice = options[randomNumber]
+    print("The computer randomly chose " + computersChoice)
 
-if(userInput == computersChoice):
-    print("You both chose " + userInput +  ", the game is a draw")
-if (userInput=="paper" and computersChoice=="rock"):
-    print("Paper covers rock, you win!!")
-if (userInput=="rock" and computersChoice=="paper"):
-    print("Paper covers rock, you lose!!")
-if (userInput=="rock" and computersChoice=="scissors"):
-    print("Rock smashes scissors, you win!!")
-if (userInput=="scissors" and computersChoice=="rock"):
-    print("Rock smashes scissors, you lose!!")
-if (userInput=="scissors" and computersChoice=="paper"):
-    print("Scissors cut paper, you win!!")
-if (userInput=="paper" and computersChoice=="scissors"):
-    print("Scissors cut paper, you lose!!")
+    if(userInput == computersChoice):
+        print("You both chose " + userInput +  ", the game is a draw")
+    if (userInput=="paper" and computersChoice=="rock"):
+        userScore+=1
+        print("Paper covers rock, you win!!")
+    if (userInput=="rock" and computersChoice=="paper"):
+        computerScore+=1
+        print("Paper covers rock, you lose!!")
+    if (userInput=="rock" and computersChoice=="scissors"):
+        userScore+=1
+        print("Rock smashes scissors, you win!!")
+    if (userInput=="scissors" and computersChoice=="rock"):
+        computerScore+=1
+        print("Rock smashes scissors, you lose!!")
+    if (userInput=="scissors" and computersChoice=="paper"):
+        userScore+=1
+        print("Scissors cut paper, you win!!")
+    if (userInput=="paper" and computersChoice=="scissors"):
+        computerScore+=1
+        print("Scissors cut paper, you lose!!")
+
+print("User score: " + str(userScore))
+print("Computer score: " + str(computerScore))
+if(userScore == 3):
+    print("USER WINS 3 GAMES")
+if(computerScore==3):
+    print("COMPUTER WINS 3 GAMES")
+
--- readme.md
+++ readme.md
@@ -1,7 +1,8 @@
 ## Rock Paper Scissors
+Best of 3 command line Rock, Paper, Scissors game
 
 #### Demo
-![](RPS.gif)
+![](RPS.jpg)
 
 #### Instructions
 Use by running `python3 RPS.py`
